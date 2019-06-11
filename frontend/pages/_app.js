61404071ec0ce8048b0c131944018a77635b5973
Lee Seungmin <ehyunn@icloud.com>
2019-06-11 17:06:38 +0900

    first commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,48 @@
+import Layout from "../components/Layout";
+import withData from "../lib/apollo";
+
+import App, { Container } from "next/app";
+import React from "react";
+
+class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, isAuthenticated, ctx } = this.props;
+    return (
+      <Container>
+        <Layout isAuthenticated={isAuthenticated} {...pageProps}>
+          <Component {...props} />pageProps
+        </Layout>
+
+        <style jsx global>
+          {`
+            a {
+              color: white !important;
+            }
+            a:link {
+              text-decoration: none !important;
+              color: white !important;
+            }
+            a:hover {
+              color: white;
+            }
+            .card {
+              display: inline-block !important;
+            }
+            .card-columns {
+              column-count: 3;
+            }
+          `}
+        </style>
+      </Container>
+    );
+  }
+}
+export default withData(MyApp);
